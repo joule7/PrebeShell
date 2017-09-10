34d493d24962e6b5ca887d6b6bb91a62c12c88de
Julio César Martínez <31251600+joule7@users.noreply.github.com>
2017-09-10 12:03:39 -0500

    Definición de los casos de los comandos
    caos definidos a partir de variable que comparamos.

--- PrebeShell.sh
+++ PrebeShell.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+# Programa PrebeShell.sh
+#Autores EduardoGueva & joule7
 
+#Impedimos salir con Ctrl-z y Ctrl-c
 trap 'salir' 2 20
+trap 'salir' SIGTSTP
 
 salir()
 {
-	echo -e "\nNo sirve"
+	echo -e "\nNo sirve tal accion, escriba: salir"
 }
-
 printf "\033c"
 
+#Contador para los intentos
 intentos=0
 
 while [ "$intentos" != 3 ]; do
@@ -38,37 +42,37 @@ while [ "$intentos" != 3 ]; do
 		printf "\033c"
 		echo -e -n "[31mPREBESHELL[00m"
 		printf "\033c"
-
+		#Lectura y accion de Comandos
 		while [ "$comando"  != 'salir' ];do 
 		echo  -e -n "$usuario$PWD-->"
-		read  comando
-		case "$comando" in
-		
-		ayuda)
-		;;
-		cls)
-		;;
-		infosis)
-		;;
-		arbol)
-		;;
-		fecha)
-		;;
-		hora)
-		;;
-		busca)
-		;;
-		Juego1)
-		;;
-		Juego2)
-		;;
-		creditos)
-		;;
-		prebeplayer)
-		;;
-        salir)
-            exit 1
-        ;;
+		read  comando #Nuestra variable comparativa
+		if [ "$comando" == "arbol" ];then
+			#
+		elif [ "$comando" == "ayuda" ];then
+			#
+		elif [ "$comando" == "infosis" ];then
+			#
+		elif [ "$comando" == "fecha" ];then
+			#
+		elif [ "$comando" == "hora" ];then
+			#
+		elif [ "$comando" == "buscar" ];then
+			#
+		elif [ "$comando" == "juego1" ];then
+			#
+		elif [ "$comando" == "juego2" ];then
+			#
+		elif [ "$comando" == "prebeplayer" ];then
+			#
+		elif [ "$comando" == "creditos" ];then
+			#
+		elif [ "$comando" == "cls" ];then
+			#
+		elif [ "$comando" == "salir" ];then
+			exit 1
+		else
+			$comando
+		fi
 		*)
 			echo -e "\n$comando: no se reconoce la orden\n"
 		;;
