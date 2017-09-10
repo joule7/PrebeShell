c29963c1ee5b1c455a20dedb8fc1c435a4b35fd2
Julio César Martínez <31251600+joule7@users.noreply.github.com>
2017-09-10 17:17:52 -0500

    Definicion de la mayoria de los comandos de nuestra PrebeShell
    Se opto por desarrollar los comandos generales en este script, que funge como main con esas funciones generales. 
    Los juegos y la prebeplayer serán desarrollados en archivos aparte

--- PrebeShell.sh
+++ PrebeShell.sh
@@ -8,7 +8,8 @@ trap 'salir' SIGTSTP
 
 salir()
 {
-	echo -e "\nNo sirve tal accion, escriba: salir"
+	echo -e "\nNo sirve tal accion, escriba: salir -> termina el programa"
+	echo -e "\nEn su defecto ayuda -> para ver los comandos"
 }
 printf "\033c"
 
@@ -16,6 +17,8 @@ printf "\033c"
 intentos=0
 
 while [ "$intentos" != 3 ]; do
+	echo -e "\nBienvenido $USER!!! estas en la ..::PrebeShell::.."
+	echo -e "\nfavor de ingresar:"
 	echo -e "\nUsuario:"
 	read usuario  
 	echo -e "\nContrasena:"
@@ -40,34 +43,70 @@ while [ "$intentos" != 3 ]; do
 		if [[ $con == *$contra* ]];then 
 
 		printf "\033c"
-		echo -e -n "[31mPREBESHELL[00m"
-		printf "\033c"
+		echo -e -n "\e[32m..::PREBESHELL::..\e[00m" #parte del prompt
 		#Lectura y accion de Comandos
 		while [ "$comando"  != 'salir' ];do 
 		echo  -e -n "$usuario$PWD-->" #pwd nos muestra la ruta en que el usuario esta actualmente
 		read  comando #Nuestra variable comparativa
 		if [ "$comando" == "arbol" ];then
-			#
+			echo -e "\n\e[32m..::Árbol::..\e[0m\n"
+			tree
 		elif [ "$comando" == "ayuda" ];then
-			#
+			echo -e "\n\e[32m..::Información de Comandos::..\e[0m\n"
+			echo -e "\e[32minfosis:\e[0m Presenta la información del sistema"
+			echo -e "\e[32mfecha:\e[0m Muestra la fecha actual"
+			echo -e "\e[32mhora:\e[0m Muestra la hora actual"
+			echo -e "\e[32mbuscar:\e[0m Busca un archivo escribir: archivo directorio"
+			echo -e "\e[32marbol:\e[0m Presenta una simulación del comando tree"
+			echo -e "\e[32mprebeplayer:\e[0m Reproductor PrebePlayer"
+			echo -e "\e[32mGH:\e[0m Guitar Hero (juego)"
+			echo -e "\e[32mjuego2:\e[0m Juego 2"
+			echo -e "\e[32mcreditos:\e[0m Nombre de los programadores"
+			echo -e "\e[32mcls:\e[0m limpia la pantalla\n"
 		elif [ "$comando" == "infosis" ];then
-			#
+			echo -e "\n\e[32m..::Información del Sistema::..\e[0m\n"
+			echo -n -e "\e[32mFabricante Equipo: \e[0m"
+			sudo dmidecode -s system-manufacturer
+			echo -n -e "\e[32mNombre del Producto: \e[0m"
+			sudo dmidecode -s system-product-name
+			echo -n -e "\e[32mEtiqueta de servicio: \e[0m"
+			sudo dmidecode -s system-version
+			echo -n -e "\e[32mNúmero de Serie del Equipo: \e[0m"
+			sudo dmidecode -s system-serial-number
+			echo -n -e "\e[32mFabricante procesador: \e[0m"
+			sudo dmidecode -s processor-manufacturer
+			echo -n -e "\e[32mArquitectura: \e[0m"
+			uname -m
+			echo -n -e "\e[32mDatos Procesador: \e[0m"
+			grep 'model name' /proc/cpuinfo
+			echo -n -e "\e[32mKernel: \e[0m"
+			uname -o
+			echo -n -e "\e[32mDistribución: \e[0m"
+			lsb_release -idc
+			echo -n -e "\e[32mUsuario Actual: \e[0m"
+			whoami			
+			echo -e "\e[32mCarcaterísticas Unidades Almacenamiento: \e[0m"
+			df -h
 		elif [ "$comando" == "fecha" ];then
-			#
+			echo -e "\n\e[32m..::Fecha::..\e[0m"
+			date "+%nHoy es: %Y-%m-%d%n"
 		elif [ "$comando" == "hora" ];then
-			#
+			echo -e "\n\e[32m..::Hora::..\e[0m"
+			date "+%nLa hora es: %H:%M:%S%n"
 		elif [ "$comando" == "buscar" ];then
-			#
-		elif [ "$comando" == "juego1" ];then
-			#
+			echo "funcion comando buscar en proceso"
+		elif [ "$comando" == "GH" ];then
+			cd /bin/bash ./GH.sh
 		elif [ "$comando" == "juego2" ];then
-			#
+			cd /bin/bash ./juego2.sh
 		elif [ "$comando" == "prebeplayer" ];then
-			#
+			cd /bin/bash ./prebeplayer.sh
 		elif [ "$comando" == "creditos" ];then
-			#
+			echo -e "\n\e[32m..::Créditos::..\e[0m\n"
+			echo -e "Gueva Eduardo"
+			echo -e "Martinez Troncoso Julio Cesar\n"
 		elif [ "$comando" == "cls" ];then
-			#
+			printf "\033c"
 		elif [ "$comando" == "salir" ];then
 			exit 1
 		else
